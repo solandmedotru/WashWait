6091696927ff2cb5a58b747e35ed87850f9fb7ab
Solomin Andrey <solomin.andrey@gmail.com>
2016-11-06 19:48:05 +0300

    refactoring, create simple main layout

--- app/src/main/java/ru/solandme/washwait/POJO/Sys.java
+++ app/src/main/java/ru/solandme/washwait/POJO/Sys.java
@@ -6,26 +6,131 @@ import com.google.gson.annotations.SerializedName;
 
 public class Sys {
 
-    @SerializedName("population")
+    @SerializedName("type")
     @Expose
-    private long population;
+    private int type;
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("message")
+    @Expose
+    private double message;
+    @SerializedName("country")
+    @Expose
+    private String country;
+    @SerializedName("sunrise")
+    @Expose
+    private int sunrise;
+    @SerializedName("sunset")
+    @Expose
+    private int sunset;
 
     /**
-     * 
+     *
      * @return
-     *     The population
+     *     The type
      */
-    public long getPopulation() {
-        return population;
+    public int getType() {
+        return type;
     }
 
     /**
-     * 
-     * @param population
-     *     The population
+     *
+     * @param type
+     *     The type
      */
-    public void setPopulation(long population) {
-        this.population = population;
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    /**
+     *
+     * @return
+     *     The id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     *     The id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     *     The message
+     */
+    public double getMessage() {
+        return message;
+    }
+
+    /**
+     *
+     * @param message
+     *     The message
+     */
+    public void setMessage(double message) {
+        this.message = message;
+    }
+
+    /**
+     *
+     * @return
+     *     The country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     *
+     * @param country
+     *     The country
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     *
+     * @return
+     *     The sunrise
+     */
+    public int getSunrise() {
+        return sunrise;
+    }
+
+    /**
+     *
+     * @param sunrise
+     *     The sunrise
+     */
+    public void setSunrise(int sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    /**
+     *
+     * @return
+     *     The sunset
+     */
+    public int getSunset() {
+        return sunset;
+    }
+
+    /**
+     *
+     * @param sunset
+     *     The sunset
+     */
+    public void setSunset(int sunset) {
+        this.sunset = sunset;
     }
 
 }
--- app/src/main/java/ru/solandme/washwait/WeatherFragment.java
+++ app/src/main/java/ru/solandme/washwait/WeatherFragment.java
@@ -46,6 +46,16 @@ public class WeatherFragment extends Fragment implements SwipeRefreshLayout.OnRe
     private TextView weatherIcon;
     private TextView forecast;
 
+    private TextView forecastDay1;
+    private TextView forecastDay2;
+    private TextView forecastDay3;
+    private TextView forecastDay4;
+
+    private TextView forecastDate1;
+    private TextView forecastDate2;
+    private TextView forecastDate3;
+    private TextView forecastDate4;
+
     private String lat = "35";
     private String lon = "139";
     private String cnt = "10";
@@ -88,6 +98,20 @@ public class WeatherFragment extends Fragment implements SwipeRefreshLayout.OnRe
         detailsField = (TextView) rootView.findViewById(R.id.details_field);
         currentTemperatureField = (TextView) rootView.findViewById(R.id.current_temperature_field);
         forecast = (TextView) rootView.findViewById(R.id.forecast);
+
+        forecastDay1 = (TextView) rootView.findViewById(R.id.forecast_1);
+        forecastDay1.setTypeface(weatherFont);
+        forecastDay2 = (TextView) rootView.findViewById(R.id.forecast_2);
+        forecastDay2.setTypeface(weatherFont);
+        forecastDay3 = (TextView) rootView.findViewById(R.id.forecast_3);
+        forecastDay3.setTypeface(weatherFont);
+        forecastDay4 = (TextView) rootView.findViewById(R.id.forecast_4);
+        forecastDay4.setTypeface(weatherFont);
+
+        forecastDate1 = (TextView) rootView.findViewById(R.id.date1);
+        forecastDate2 = (TextView) rootView.findViewById(R.id.date2);
+        forecastDate3 = (TextView) rootView.findViewById(R.id.date3);
+        forecastDate4 = (TextView) rootView.findViewById(R.id.date4);
 
         weatherIcon = (TextView) rootView.findViewById(R.id.weather_icon);
         weatherIcon.setTypeface(weatherFont);
@@ -151,6 +175,7 @@ public class WeatherFragment extends Fragment implements SwipeRefreshLayout.OnRe
             public void onResponse(Call<WeatherFiveDays> call, Response<WeatherFiveDays> response) {
                 if (response.isSuccessful()) {
                     updateWashForecastUI(response.body());
+                    updateFiveDaysForecastUI(response.body());
                 }
             }
 
@@ -162,6 +187,38 @@ public class WeatherFragment extends Fragment implements SwipeRefreshLayout.OnRe
         });
     }
 
+    private void updateFiveDaysForecastUI(WeatherFiveDays body) {
+        String icon1 = body.getList().get(3).getWeather().get(0).getIcon();
+        String icon2 = body.getList().get(11).getWeather().get(0).getIcon();
+        String icon3 = body.getList().get(19).getWeather().get(0).getIcon();
+        String icon4 = body.getList().get(27).getWeather().get(0).getIcon();
+
+        Long data1 = body.getList().get(3).getDt();
+        Log.e(TAG, "onError: " + data1);
+        Long data2 = body.getList().get(11).getDt();
+        Log.e(TAG, "onError: " + data2);
+        Long data3 = body.getList().get(19).getDt();
+        Log.e(TAG, "onError: " + data3);
+        Long data4 = body.getList().get(27).getDt();
+        Log.e(TAG, "onError: " + data4);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EE, dd", Locale.getDefault());
+        String data11 = dateFormat.format(data1 * 1000);
+        String data21 = dateFormat.format(data2 * 1000);
+        String data31 = dateFormat.format(data3 * 1000);
+        String data41 = dateFormat.format(data4 * 1000);
+
+        forecastDay1.setText(getIconFont(icon1));
+        forecastDay2.setText(getIconFont(icon2));
+        forecastDay3.setText(getIconFont(icon3));
+        forecastDay4.setText(getIconFont(icon4));
+
+        forecastDate1.setText(data11);
+        forecastDate2.setText(data21);
+        forecastDate3.setText(data31);
+        forecastDate4.setText(data41);
+    }
+
     private void updateWashForecastUI(WeatherFiveDays weatherFiveDays) {
         String forecastText = WashCar.getForecastText(getContext(), weatherFiveDays, FORECAST_DISTANCE);
         forecast.setText(forecastText);
@@ -170,17 +227,16 @@ public class WeatherFragment extends Fragment implements SwipeRefreshLayout.OnRe
     private void updateWeatherUI(CurrentWeather currentWeather) {
 
         String cityName = currentWeather.getName();
+        String country = currentWeather.getSys().getCountry();
         Long dt = (long) currentWeather.getDt();
         Long humidity = currentWeather.getMain().getHumidity();
         double temp = currentWeather.getMain().getTemp();
-        double pressure = currentWeather.getMain().getPressure();
 
-        cityField.setText(cityName);
+        cityField.setText(cityName + ", " + country);
         detailsField.setTypeface(weatherFont);
         currentTemperatureField.setTypeface(weatherFont);
         detailsField.setText(currentWeather.getWeather().get(0).getDescription().toUpperCase() +
-                "\n" + getString(R.string.wi_humidity) + " " + getString(R.string.humidity) + humidity + "%" +
-                "\n" + getString(R.string.wi_barometer) + " " + getString(R.string.pressure) + pressure + " hPa");
+                "\n" + getString(R.string.wi_humidity) + " " + getString(R.string.humidity) + humidity + "%" );
 
         String unitTemperature;
         switch (units) {
@@ -207,62 +263,10 @@ public class WeatherFragment extends Fragment implements SwipeRefreshLayout.OnRe
                 getString(R.string.last_update),
                 updatedOn));
         Log.e(TAG, "updateWeatherUI: " + currentWeather.getWeather().get(0).getIcon() );
-        switch (currentWeather.getWeather().get(0).getIcon()) {
-            case "01d":
-                weatherIcon.setText(R.string.wi_day_sunny);
-                break;
-            case "01n":
-                weatherIcon.setText(R.string.wi_night_clear);
-                break;
-            case "02d":
-                weatherIcon.setText(R.string.wi_day_cloudy);
-                break;
-            case "02n":
-                weatherIcon.setText(R.string.wi_night_cloudy);
-                break;
-            case "03d":
-                weatherIcon.setText(R.string.wi_cloud);
-                break;
-            case "03n":
-                weatherIcon.setText(R.string.wi_cloud);
-                break;
-            case "04d":
-                weatherIcon.setText(R.string.wi_cloudy);
-                break;
-            case "04n":
-                weatherIcon.setText(R.string.wi_cloudy);
-                break;
-            case "09d":
-                weatherIcon.setText(R.string.wi_day_showers);
-                break;
-            case "09n":
-                weatherIcon.setText(R.string.wi_night_showers);
-                break;
-            case "10d":
-                weatherIcon.setText(R.string.wi_day_rain_mix);
-                break;
-            case "10n":
-                weatherIcon.setText(R.string.wi_night_rain_mix);
-                break;
-            case "11d":
-                weatherIcon.setText(R.string.wi_day_rain);
-                break;
-            case "11n":
-                weatherIcon.setText(R.string.wi_night_rain);
-                break;
-            case "13d":
-                weatherIcon.setText(R.string.wi_day_snow);
-                break;
-            case "13n":
-                weatherIcon.setText(R.string.wi_night_snow);
-                break;
-            case "50d":
-                weatherIcon.setText(R.string.wi_day_fog);
-                break;
-            case "50n":
-                weatherIcon.setText(R.string.wi_night_fog);
-                break;
-        }
+
+        weatherIcon.setText(getIconFont(currentWeather.getWeather().get(0).getIcon()));
+
+
     }
 
     @Override
@@ -270,5 +274,49 @@ public class WeatherFragment extends Fragment implements SwipeRefreshLayout.OnRe
         swipeRefreshLayout.setRefreshing(true);
         getWeather();
         swipeRefreshLayout.setRefreshing(false);
+    }
+
+    public int getIconFont(String icon) {
+
+        switch (icon) {
+            case "01d":
+                return R.string.wi_day_sunny;
+            case "01n":
+                return R.string.wi_night_clear;
+            case "02d":
+                return R.string.wi_day_cloudy;
+            case "02n":
+                return R.string.wi_night_cloudy;
+            case "03d":
+                return R.string.wi_cloud;
+            case "03n":
+                return R.string.wi_cloud;
+            case "04d":
+                return R.string.wi_cloudy;
+            case "04n":
+                return R.string.wi_cloudy;
+            case "09d":
+                return R.string.wi_day_showers;
+            case "09n":
+                return R.string.wi_night_showers;
+            case "10d":
+                return R.string.wi_day_rain_mix;
+            case "10n":
+                return R.string.wi_night_rain_mix;
+            case "11d":
+                return R.string.wi_day_rain;
+            case "11n":
+                return R.string.wi_night_rain;
+            case "13d":
+                return R.string.wi_day_snow;
+            case "13n":
+                return R.string.wi_night_snow;
+            case "50d":
+                return R.string.wi_day_fog;
+            case "50n":
+                return R.string.wi_night_fog;
+            default:
+                return R.string.wi_day_sunny;
+        }
     }
 }
