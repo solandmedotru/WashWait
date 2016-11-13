19559815c7467b69ab939f358b9138f1f7deb140
Solomin Andrey <solomin.andrey@gmail.com>
2016-11-13 19:53:02 +0300

    added binding setting value

--- app/src/main/java/ru/solandme/washwait/SettingsActivity.java
+++ app/src/main/java/ru/solandme/washwait/SettingsActivity.java
@@ -1,8 +1,11 @@
 package ru.solandme.washwait;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
     @Override
@@ -10,13 +13,48 @@ public class SettingsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
 
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment {
+    public static class MyPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.my_preference_screen);
+
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_city_key)));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_limit_key)));
+
         }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+
+            onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getString(preference.getKey(), ""));
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    preference.setSummary(listPreference.getEntries()[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+
+            return true;
+        }
+
+
     }
 }
