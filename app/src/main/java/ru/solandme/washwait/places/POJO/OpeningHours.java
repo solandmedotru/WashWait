e6eb7401bb1be54c02e2c1e80d1e70e035c99b40
Andrey Solomin <solomin.andrey@gmail.com>
2016-12-08 01:44:24 +0300

    design experiment with map layout

---
+++ app/src/main/java/ru/solandme/washwait/places/POJO/OpeningHours.java
@@ -0,0 +1,74 @@
+
+package ru.solandme.washwait.places.POJO;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class OpeningHours {
+
+    @SerializedName("open_now")
+    @Expose
+    private boolean openNow;
+    @SerializedName("periods")
+    @Expose
+    private List<Period> periods = null;
+    @SerializedName("weekday_text")
+    @Expose
+    private List<String> weekdayText = null;
+
+    /**
+     * 
+     * @return
+     *     The openNow
+     */
+    public boolean isOpenNow() {
+        return openNow;
+    }
+
+    /**
+     * 
+     * @param openNow
+     *     The open_now
+     */
+    public void setOpenNow(boolean openNow) {
+        this.openNow = openNow;
+    }
+
+    /**
+     * 
+     * @return
+     *     The periods
+     */
+    public List<Period> getPeriods() {
+        return periods;
+    }
+
+    /**
+     * 
+     * @param periods
+     *     The periods
+     */
+    public void setPeriods(List<Period> periods) {
+        this.periods = periods;
+    }
+
+    /**
+     * 
+     * @return
+     *     The weekdayText
+     */
+    public List<String> getWeekdayText() {
+        return weekdayText;
+    }
+
+    /**
+     * 
+     * @param weekdayText
+     *     The weekday_text
+     */
+    public void setWeekdayText(List<String> weekdayText) {
+        this.weekdayText = weekdayText;
+    }
+
+}
---
+++ app/src/main/java/ru/solandme/washwait/places/POJO/Result.java
@@ -0,0 +1,452 @@
+
+package ru.solandme.washwait.places.POJO;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Result {
+
+    @SerializedName("address_components")
+    @Expose
+    private List<AddressComponent> addressComponents = null;
+    @SerializedName("adr_address")
+    @Expose
+    private String adrAddress;
+    @SerializedName("formatted_address")
+    @Expose
+    private String formattedAddress;
+    @SerializedName("formatted_phone_number")
+    @Expose
+    private String formattedPhoneNumber;
+    @SerializedName("geometry")
+    @Expose
+    private Geometry geometry;
+    @SerializedName("icon")
+    @Expose
+    private String icon;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("international_phone_number")
+    @Expose
+    private String internationalPhoneNumber;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("opening_hours")
+    @Expose
+    private OpeningHours openingHours;
+    @SerializedName("photos")
+    @Expose
+    private List<Photo> photos = null;
+    @SerializedName("place_id")
+    @Expose
+    private String placeId;
+    @SerializedName("rating")
+    @Expose
+    private double rating;
+    @SerializedName("reference")
+    @Expose
+    private String reference;
+    @SerializedName("reviews")
+    @Expose
+    private List<Review> reviews = null;
+    @SerializedName("scope")
+    @Expose
+    private String scope;
+    @SerializedName("types")
+    @Expose
+    private List<String> types = null;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("utc_offset")
+    @Expose
+    private int utcOffset;
+    @SerializedName("vicinity")
+    @Expose
+    private String vicinity;
+    @SerializedName("website")
+    @Expose
+    private String website;
+
+    /**
+     * 
+     * @return
+     *     The addressComponents
+     */
+    public List<AddressComponent> getAddressComponents() {
+        return addressComponents;
+    }
+
+    /**
+     * 
+     * @param addressComponents
+     *     The address_components
+     */
+    public void setAddressComponents(List<AddressComponent> addressComponents) {
+        this.addressComponents = addressComponents;
+    }
+
+    /**
+     * 
+     * @return
+     *     The adrAddress
+     */
+    public String getAdrAddress() {
+        return adrAddress;
+    }
+
+    /**
+     * 
+     * @param adrAddress
+     *     The adr_address
+     */
+    public void setAdrAddress(String adrAddress) {
+        this.adrAddress = adrAddress;
+    }
+
+    /**
+     * 
+     * @return
+     *     The formattedAddress
+     */
+    public String getFormattedAddress() {
+        return formattedAddress;
+    }
+
+    /**
+     * 
+     * @param formattedAddress
+     *     The formatted_address
+     */
+    public void setFormattedAddress(String formattedAddress) {
+        this.formattedAddress = formattedAddress;
+    }
+
+    /**
+     * 
+     * @return
+     *     The formattedPhoneNumber
+     */
+    public String getFormattedPhoneNumber() {
+        return formattedPhoneNumber;
+    }
+
+    /**
+     * 
+     * @param formattedPhoneNumber
+     *     The formatted_phone_number
+     */
+    public void setFormattedPhoneNumber(String formattedPhoneNumber) {
+        this.formattedPhoneNumber = formattedPhoneNumber;
+    }
+
+    /**
+     * 
+     * @return
+     *     The geometry
+     */
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    /**
+     * 
+     * @param geometry
+     *     The geometry
+     */
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    /**
+     * 
+     * @return
+     *     The icon
+     */
+    public String getIcon() {
+        return icon;
+    }
+
+    /**
+     * 
+     * @param icon
+     *     The icon
+     */
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    /**
+     * 
+     * @return
+     *     The id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @param id
+     *     The id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 
+     * @return
+     *     The internationalPhoneNumber
+     */
+    public String getInternationalPhoneNumber() {
+        return internationalPhoneNumber;
+    }
+
+    /**
+     * 
+     * @param internationalPhoneNumber
+     *     The international_phone_number
+     */
+    public void setInternationalPhoneNumber(String internationalPhoneNumber) {
+        this.internationalPhoneNumber = internationalPhoneNumber;
+    }
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * @return
+     *     The openingHours
+     */
+    public OpeningHours getOpeningHours() {
+        return openingHours;
+    }
+
+    /**
+     * 
+     * @param openingHours
+     *     The opening_hours
+     */
+    public void setOpeningHours(OpeningHours openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    /**
+     * 
+     * @return
+     *     The photos
+     */
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    /**
+     * 
+     * @param photos
+     *     The photos
+     */
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    /**
+     * 
+     * @return
+     *     The placeId
+     */
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    /**
+     * 
+     * @param placeId
+     *     The place_id
+     */
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    /**
+     * 
+     * @return
+     *     The rating
+     */
+    public double getRating() {
+        return rating;
+    }
+
+    /**
+     * 
+     * @param rating
+     *     The rating
+     */
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    /**
+     * 
+     * @return
+     *     The reference
+     */
+    public String getReference() {
+        return reference;
+    }
+
+    /**
+     * 
+     * @param reference
+     *     The reference
+     */
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    /**
+     * 
+     * @return
+     *     The reviews
+     */
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    /**
+     * 
+     * @param reviews
+     *     The reviews
+     */
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    /**
+     * 
+     * @return
+     *     The scope
+     */
+    public String getScope() {
+        return scope;
+    }
+
+    /**
+     * 
+     * @param scope
+     *     The scope
+     */
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    /**
+     * 
+     * @return
+     *     The types
+     */
+    public List<String> getTypes() {
+        return types;
+    }
+
+    /**
+     * 
+     * @param types
+     *     The types
+     */
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
+
+    /**
+     * 
+     * @return
+     *     The url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * 
+     * @param url
+     *     The url
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * 
+     * @return
+     *     The utcOffset
+     */
+    public int getUtcOffset() {
+        return utcOffset;
+    }
+
+    /**
+     * 
+     * @param utcOffset
+     *     The utc_offset
+     */
+    public void setUtcOffset(int utcOffset) {
+        this.utcOffset = utcOffset;
+    }
+
+    /**
+     * 
+     * @return
+     *     The vicinity
+     */
+    public String getVicinity() {
+        return vicinity;
+    }
+
+    /**
+     * 
+     * @param vicinity
+     *     The vicinity
+     */
+    public void setVicinity(String vicinity) {
+        this.vicinity = vicinity;
+    }
+
+    /**
+     * 
+     * @return
+     *     The website
+     */
+    public String getWebsite() {
+        return website;
+    }
+
+    /**
+     * 
+     * @param website
+     *     The website
+     */
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+}
