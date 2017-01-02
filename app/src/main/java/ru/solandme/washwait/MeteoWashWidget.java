2b38492e4397e9fa85015dcbbe3462756d56b9d4
Andrey Solomin <solomin.andrey@gmail.com>
2017-01-02 12:45:13 +0300

    refactoring

--- app/src/main/java/ru/solandme/washwait/MeteoWashWidget.java
+++ app/src/main/java/ru/solandme/washwait/MeteoWashWidget.java
@@ -14,79 +14,80 @@ import ru.solandme.washwait.utils.WeatherUtils;
 
 public class MeteoWashWidget extends AppWidgetProvider {
 
-  static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
-    int textColor = sharedPref.getInt("pref_textColor_key", Color.GRAY);
-    int bgColor = sharedPref.getInt("pref_bgColor_key", Color.BLACK);
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        int textColor = sharedPref.getInt("pref_textColor_key", Color.GRAY);
+        int bgColor = sharedPref.getInt("pref_bgColor_key", Color.BLACK);
 
-    // Construct the RemoteViews object
-    RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.meteo_wash_widget);
+        // Construct the RemoteViews object
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.meteo_wash_widget);
 
-    Intent intent = new Intent(context, MainActivity.class);
-    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-    remoteViews.setOnClickPendingIntent(R.id.widgetContent, pendingIntent);
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        remoteViews.setOnClickPendingIntent(R.id.widgetContent, pendingIntent);
 
-    String units = sharedPref.getString("units", ForecastService.DEFAULT_UNITS);
-    double maxTemp = Double.parseDouble(sharedPref.getString("pref_maxTemp_key", "0"));
-    double minTemp = Double.parseDouble(sharedPref.getString("pref_minTemp_key", "0"));
-    String description = sharedPref.getString("pref_description_key", "");
-    int icon = Integer.parseInt(
-        sharedPref.getString("pref_icon_key", String.valueOf(R.mipmap.broken_clouds)));
-    int humidity = Integer.parseInt(sharedPref.getString("pref_humidity_key", "0"));
-    double barometer = Double.parseDouble(sharedPref.getString("pref_barometer_key", "0"));
-    double speedWind = Double.parseDouble(sharedPref.getString("pref_speedWind_key", "0"));
-    int speedDirection = Integer.parseInt(sharedPref.getString("pref_speedDirection_key", "0"));
-    String textForWashForecast = sharedPref.getString("pref_text_to_wash_key", "");
+        String units = sharedPref.getString("units", ForecastService.DEFAULT_UNITS);
+        double maxTemp = Double.parseDouble(sharedPref.getString("pref_maxTemp_key", "0"));
+        double minTemp = Double.parseDouble(sharedPref.getString("pref_minTemp_key", "0"));
+        String description = sharedPref.getString("pref_description_key", "");
+        int icon = Integer.parseInt(
+                sharedPref.getString("pref_icon_key", String.valueOf(R.mipmap.broken_clouds)));
+        int humidity = Integer.parseInt(sharedPref.getString("pref_humidity_key", "0"));
+        double barometer = Double.parseDouble(sharedPref.getString("pref_barometer_key", "0"));
+        double speedWind = Double.parseDouble(sharedPref.getString("pref_speedWind_key", "0"));
+        int speedDirection = Integer.parseInt(sharedPref.getString("pref_speedDirection_key", "0"));
+        String textForWashForecast = sharedPref.getString("pref_text_to_wash_key", "");
 
-    remoteViews = fillWidget(context, textColor, bgColor, remoteViews, units, maxTemp, minTemp, description, icon,
-            humidity, barometer, speedWind, speedDirection, textForWashForecast);
+        remoteViews = fillWidget(context, textColor, bgColor, remoteViews, units, maxTemp, minTemp, description, icon,
+                humidity, barometer, speedWind, speedDirection, textForWashForecast);
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
-  }
-
-  public static RemoteViews fillWidget(Context context, int textColor, int bgColor, RemoteViews remoteViews,
-      String units, double maxTemp, double minTemp, String description, int icon, int humidity,
-      double barometer, double speedWind, int speedDirection, String textForWashForecast) {
-    remoteViews.setImageViewResource(R.id.weather_icon_day0, icon);
-    remoteViews.setImageViewBitmap(R.id.max_t_field, WeatherUtils.getFontBitmap(context,
-        WeatherUtils.getStringTemperature(maxTemp, units, context), textColor, 24));
-    remoteViews.setImageViewBitmap(R.id.separator,
-        WeatherUtils.getFontBitmap(context, " | ", textColor, 20));
-    remoteViews.setImageViewBitmap(R.id.min_t_field, WeatherUtils.getFontBitmap(context,
-        WeatherUtils.getStringTemperature(minTemp, units, context), textColor, 24));
-    remoteViews.setImageViewBitmap(R.id.barometer_field, WeatherUtils.getFontBitmap(context,
-        WeatherUtils.getStringBarometer(barometer, units, context), textColor, 14));
-    remoteViews.setImageViewBitmap(R.id.speed_wind_field, WeatherUtils.getFontBitmap(context,
-        WeatherUtils.getStringWind(speedDirection, speedWind, units, context), textColor, 14));
-    remoteViews.setImageViewBitmap(R.id.humidity_field, WeatherUtils.getFontBitmap(context,
-        (context.getString(R.string.wi_humidity) + " " + humidity + "%"), textColor, 14));
-
-    remoteViews.setTextViewText(R.id.details_field, description);
-    remoteViews.setTextColor(R.id.details_field, textColor);
-    remoteViews.setTextViewText(R.id.forecast_message, textForWashForecast);
-    remoteViews.setTextColor(R.id.forecast_message, textColor);
-
-    remoteViews.setInt(R.id.widgetContent, "setBackgroundColor", bgColor);
-    return remoteViews;
-  }
-
-  @Override
-  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-    // There may be multiple widgets active, so update all of them
-    ForecastService.startActionGetForecast(context, ForecastService.RUN_FROM_ACTIVITY);
-    for (int appWidgetId : appWidgetIds) {
-      updateAppWidget(context, appWidgetManager, appWidgetId);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
     }
-  }
 
-  @Override public void onEnabled(Context context) {
-    // Enter relevant functionality for when the first widget is created
-    ForecastService.startActionGetForecast(context, ForecastService.RUN_FROM_ACTIVITY);
-  }
+    public static RemoteViews fillWidget(Context context, int textColor, int bgColor, RemoteViews remoteViews,
+                                         String units, double maxTemp, double minTemp, String description, int icon, int humidity,
+                                         double barometer, double speedWind, int speedDirection, String textForWashForecast) {
+        remoteViews.setImageViewResource(R.id.weather_icon_day0, icon);
+        remoteViews.setImageViewBitmap(R.id.max_t_field, WeatherUtils.getFontBitmap(context,
+                WeatherUtils.getStringTemperature(maxTemp, units, context), textColor, 34));
+        remoteViews.setTextViewText(R.id.separator, " | ");
+        remoteViews.setTextColor(R.id.separator, textColor);
+        remoteViews.setImageViewBitmap(R.id.min_t_field, WeatherUtils.getFontBitmap(context,
+                WeatherUtils.getStringTemperature(minTemp, units, context), textColor, 34));
+        remoteViews.setTextViewText(R.id.barometer_field, WeatherUtils.getStringBarometer(barometer, units, context).substring(1));
+        remoteViews.setTextColor(R.id.barometer_field, textColor);
+        remoteViews.setTextViewText(R.id.speed_wind_field, WeatherUtils.getStringWind(speedDirection, speedWind, units, context).substring(1));
+        remoteViews.setTextColor(R.id.speed_wind_field, textColor);
+        remoteViews.setTextViewText(R.id.humidity_field, (context.getString(R.string.wi_humidity) + " " + humidity + "%").substring(1));
+        remoteViews.setTextColor(R.id.humidity_field, textColor);
+        remoteViews.setTextViewText(R.id.details_field, description);
+        remoteViews.setTextColor(R.id.details_field, textColor);
+        remoteViews.setTextViewText(R.id.forecast_message, textForWashForecast);
+        remoteViews.setTextColor(R.id.forecast_message, textColor);
 
-  @Override public void onDisabled(Context context) {
-    // Enter relevant functionality for when the last widget is disabled
-  }
+        remoteViews.setInt(R.id.widgetContent, "setBackgroundColor", bgColor);
+        return remoteViews;
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        ForecastService.startActionGetForecast(context, ForecastService.RUN_FROM_ACTIVITY);
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+        ForecastService.startActionGetForecast(context, ForecastService.RUN_FROM_ACTIVITY);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
 }
 
