d987a598a17f5f8ba0beaecc41d99b5f0cd35096
Solomin Andrey <solomin.andrey@gmail.com>
2017-05-29 13:18:29 +0300

    fix npe

--- app/src/main/java/ru/solandme/washwait/ui/model/washForecast/WashForecast.java
+++ app/src/main/java/ru/solandme/washwait/ui/model/washForecast/WashForecast.java
@@ -13,13 +13,11 @@ public class WashForecast {
     private String text;
     private int washDayNumber;
     private Context context;
-    private MyWeatherForecast myWeatherForecast;
 
     public WashForecast(Context context, MyWeatherForecast myWeatherForecast, String forecastDistance, float precipitationLimit) {
         this.context = context;
-        this.myWeatherForecast = myWeatherForecast;
-        this.washDayNumber = getWashDayByDistance(forecastDistance, precipitationLimit);
-        this.text = getTextForWashForecast(washDayNumber, getWashData(washDayNumber));
+        this.washDayNumber = getWashDayByDistance(forecastDistance, precipitationLimit, myWeatherForecast);
+        this.text = getTextForWashForecast(washDayNumber, getWashData(washDayNumber, myWeatherForecast));
 //        this.units = SharedPrefsUtils.getStringPreference(context, context.getString(R.string.pref_units_key), Constants.DEFAULT_UNITS);
     }
 
@@ -32,75 +30,85 @@ public class WashForecast {
     }
 
     private String getTextForWashForecast(int washDayNumber, double dataToWash) {
-        String dateToWashFormat = new SimpleDateFormat("dd MMMM, EE", Locale.getDefault()).format(dataToWash * 1000);
-        switch (washDayNumber) {
-            case 0:
-                return context.getResources().getString(R.string.can_wash);
-            case 1:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 2:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 3:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 4:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 5:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 6:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 7:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 8:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 9:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 10:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 11:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 12:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 13:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            case 14:
-                return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
-            default:
-                return context.getResources().getString(R.string.not_wash);
-        }
-    }
-
-    private double getWashData(int washDayNumber) {
-
-        if (washDayNumber >= myWeatherForecast.getMyWeatherList().size())
-            return myWeatherForecast.getMyWeatherList().get(myWeatherForecast.getMyWeatherList().size() - 1).getTime();
-        return myWeatherForecast.getMyWeatherList().get(washDayNumber).getTime();
-    }
-
-    private int getWashDayByDistance(String forecastDistance, float precipitationLimit) {
-
-        int washDayNumber = myWeatherForecast.getMaxPeriod()-1;
-        int firstDirtyDay = -1;
-        int clearDaysCounter = 0;
-        int daysCounter = 0;
-
-        for (int i = 0; i < myWeatherForecast.getMyWeatherList().size(); i++) {
-            double maxTemp = myWeatherForecast.getMyWeatherList().get(i).getTempMax();
-            float precipitation = myWeatherForecast.getMyWeatherList().get(i).getPrecipitation();
-
-            daysCounter++;
-            if (!isBadConditions(maxTemp, precipitation, precipitationLimit, myWeatherForecast.getUnits())) {
-                clearDaysCounter++;
-                if (clearDaysCounter == Integer.parseInt(forecastDistance)) {
-                    if (washDayNumber == myWeatherForecast.getMaxPeriod() - 1) {
-                        washDayNumber = daysCounter - clearDaysCounter;
-                    }
-                }
-            } else {
-                clearDaysCounter = 0;
+        if (dataToWash == 0 || washDayNumber > 14) {
+            return context.getResources().getString(R.string.not_wash);
+        } else {
+            String dateToWashFormat = new SimpleDateFormat("dd MMMM, EE", Locale.getDefault()).format(dataToWash * 1000);
+            switch (washDayNumber) {
+                case 0:
+                    return context.getResources().getString(R.string.can_wash);
+                case 1:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 2:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 3:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 4:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 5:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 6:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 7:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 8:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 9:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 10:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 11:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 12:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 13:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                case 14:
+                    return context.getResources().getString(R.string.wash, dateToWashFormat.toUpperCase());
+                default:
+                    return context.getResources().getString(R.string.not_wash);
             }
         }
+    }
 
-        Log.e(TAG, "day: " + washDayNumber + " " + firstDirtyDay);
+    private double getWashData(int washDayNumber, MyWeatherForecast myWeatherForecast) {
+
+        if (myWeatherForecast != null) {
+            if (washDayNumber >= myWeatherForecast.getMyWeatherList().size())
+                return myWeatherForecast.getMyWeatherList().get(myWeatherForecast.getMyWeatherList().size() - 1).getTime();
+            return myWeatherForecast.getMyWeatherList().get(washDayNumber).getTime();
+        } else return 0;
+
+    }
+
+    private int getWashDayByDistance(String forecastDistance, float precipitationLimit, MyWeatherForecast myWeatherForecast) {
+
+        if (myWeatherForecast != null) {
+            int washDayNumber = myWeatherForecast.getMaxPeriod() - 1;
+            int firstDirtyDay = -1;
+            int clearDaysCounter = 0;
+            int daysCounter = 0;
+
+            for (int i = 0; i < myWeatherForecast.getMyWeatherList().size(); i++) {
+                double maxTemp = myWeatherForecast.getMyWeatherList().get(i).getTempMax();
+                float precipitation = myWeatherForecast.getMyWeatherList().get(i).getPrecipitation();
+
+                daysCounter++;
+                if (!isBadConditions(maxTemp, precipitation, precipitationLimit, myWeatherForecast.getUnits())) {
+                    clearDaysCounter++;
+                    if (clearDaysCounter == Integer.parseInt(forecastDistance)) {
+                        if (washDayNumber == myWeatherForecast.getMaxPeriod() - 1) {
+                            washDayNumber = daysCounter - clearDaysCounter;
+                        }
+                    }
+                } else {
+                    clearDaysCounter = 0;
+                }
+            }
+            Log.e(TAG, "day: " + washDayNumber + " " + firstDirtyDay);
+        } else {
+            washDayNumber = 16;
+        }
         return washDayNumber;
     }
 
