6af350b50ecaa0ef7a53a121a6451cebfccded82
Solomin Andrey <solomin.andrey@gmail.com>
2017-05-30 21:00:32 +0300

    refactoring my code to MVP architecture

--- app/src/main/java/ru/solandme/washwait/versionOne/model/WeatherDate.java
+++ app/src/main/java/ru/solandme/washwait/versionOne/model/WeatherDate.java
@@ -1,4 +1,13 @@
 package ru.solandme.washwait.versionOne.model;
 
 public class WeatherDate {
+    private String currentMaxTemp;
+
+    public void setCurrentMaxTemp(String currentMaxTemp) {
+        this.currentMaxTemp = currentMaxTemp;
+    }
+
+    public String getCurrentMaxTemp() {
+        return currentMaxTemp;
+    }
 }
